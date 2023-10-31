5e3ecdad650143b607c5d7218ec1cf28b909f95f
same <132483274+koskisami@users.noreply.github.com>
2023-10-31 16:50:45 +0200

    sdf

---
+++ ch2/4_arrays.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+things = ["banaani","turtana","ananas","kaneli","unikko"]
+
+things.push("karhea")
+things.delete("kaneli")
+things.sort!
+
+puts things
---
+++ ch2/5_manipulation.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+print "Kirjoita jotain: "
+words = gets.chomp
+
+things = words.split
+
+
+
+puts things
